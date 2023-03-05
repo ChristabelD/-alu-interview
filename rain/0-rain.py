7f7e6ceb3b170e04ad4980733e801e8f8351d0fc
ChristabelD <c.adams@alustudent.com>
2023-03-05 09:49:32 -0800

    update tasks

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""
-Description: Given a list of non-negative integers representing walls of
-width 1, calculate how much water will be retained after it rains.
-- Prototype: def rain(walls)
-- walls is a list of non-negative integers.
-- Return: Integer indicating total amount of rainwater retained.
-- Assume that the ends of the list (before index 0 and after index
-  walls[-1]) are not walls, meaning they will not retain water.
-- If the list is empty return 0.
-"""
+""" Rain trap """
 
 
 def rain(walls):
-    """
-    Args:
-        walls - is a list of non-negative integers.
-    Returns:
-        If the list is empty return 0.
-    """
     n = len(walls)
-    total_water = 0
+    if n == 0:
+        return 0
 
-    for i in range(1, n - 1):
-        left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
-        right = walls[i]
+    left = 0
+    right = n-1
+    left_side = 0
+    right_side = 0
+    water = 0
 
-        for j in range(i + 1, n):
-            right = max(right, walls[j])
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_side:
+                left_side = walls[left]
+            else:
+                water += left_side - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_side:
+                right_side = walls[right]
+            else:
+                water += right_side - walls[right]
+            right -= 1
 
-        total_water = total_water + (min(left, right) - walls[i])
-    return total_wate
+    return water
